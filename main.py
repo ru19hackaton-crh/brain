b66e1563e3d2d4ddbc300ecaf9efa10d6c142ee5
Sami Saada <sami.saada@gmail.com>
2019-10-12 02:58:26 +0300

    Speed up logic

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ class RobotHandler(CommonBrainHandler):
 if __name__ == "__main__":
     brain = Brain()
 
-    brain_operating = tornado.ioloop.PeriodicCallback(brain.operate, 1000)
+    brain_operating = tornado.ioloop.PeriodicCallback(brain.operate, 10)
     application = tornado.web.Application([
         (r"/monitor", MonitorHandler, dict(brain=brain)),
         (r"/robot", RobotHandler, dict(brain=brain)),
