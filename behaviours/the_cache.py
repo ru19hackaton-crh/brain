f5055a4a8ffc2ab57fa2c489acd547acbfb77e70
Sami Saada <sami.saada@gmail.com>
2019-10-13 02:53:32 +0300

    Clean extras

--- behaviours/the_cache.py
+++ behaviours/the_cache.py
@@ -7,12 +7,6 @@ from py_trees.common import Status
 
 import json
 
-def colour_is_whitish(colour):
-    return abs(colour - 20) < 5
-
-def colour_is_yellow(colour):
-    return abs(colour - 14) < 2
-
 class EntryRunBehaviour(py_trees.behaviour.Behaviour):
 
     def __init__(self, name="Entry Run"):
@@ -37,11 +31,11 @@ class EntryRunBehaviour(py_trees.behaviour.Behaviour):
             self.blackboard.robot_response = False
         self.previous_command_sent = None
 
-class DriveOnWhiteBehaviour(py_trees.behaviour.Behaviour):
+class ActivateYellowBehaviour(py_trees.behaviour.Behaviour):
 
-    def __init__(self, name="Drive on white"):
+    def __init__(self, name="Yellow finder"):
         super().__init__(name)
-        self.blackboard.register_key("colour", read=True)
+        self.blackboard.register_key("robot_response", read=True, write=True)
 
     def setup(self, timeout, brain=None):
         if brain:
@@ -49,23 +43,16 @@ class DriveOnWhiteBehaviour(py_trees.behaviour.Behaviour):
         return True
 
     def update(self):
-        if colour_is_whitish(self.blackboard.colour):
-            if self.status != Status.RUNNING:
-                self.brain.robot.write_message("COMMAND: DRIVE_ON_WHITE")
-            return Status.RUNNING
-        else:
-            return Status.FAILURE
+        return Status.FAILURE
 
     def terminate(self, new_status):
-        if new_status == Status.FAILURE and self.status != Status.FAILURE:
-            self.brain.robot.write_message("COMMAND: STOP")
+        pass
 
-class SearchTheWhiteBehaviour(py_trees.behaviour.Behaviour):
+class FollowTheLineBehaviour(py_trees.behaviour.Behaviour):
 
-    def __init__(self, name="Search the white"):
+    def __init__(self, name="Line Following"):
         super().__init__(name)
-        self.blackboard.register_key("colour", read=True)
-        self.iter = 0
+        self.blackboard.register_key("robot_response", read=True, write=True)
 
     def setup(self, timeout, brain=None):
         if brain:
@@ -73,21 +60,13 @@ class SearchTheWhiteBehaviour(py_trees.behaviour.Behaviour):
         return True
 
     def update(self):
-        if colour_is_whitish(self.blackboard.colour):
-            if self.status == Status.RUNNING:
-                return Status.SUCCESS
-            else:
-                return Status.FAILURE
-        if self.status != Status.RUNNING or self.iter > 200:
-            self.iter = 0
-            self.brain.robot.write_message("COMMAND: FIND_WHITE")
-        self.iter += 1
+        if self.status != Status.RUNNING:
+            self.brain.robot.write_message("COMMAND: FOLLOWLINE")
         return Status.RUNNING
 
     def terminate(self, new_status):
         if new_status == Status.SUCCESS:
-            self.iter = 0
-            self.brain.robot.write_message("COMMAND: STOP")
+            self.brain.robot.write_message("COMMAND: STOPFOLLOWLINE")
 
 def create_the_cache_subtree(brain):
     the_cache = py_trees.composites.Sequence("The Cache")
@@ -95,10 +74,11 @@ def create_the_cache_subtree(brain):
     entry_run.setup(0, brain)
     entry_run_oneshot = py_trees.decorators.OneShot(entry_run)
     follow_the_line = py_trees.composites.Selector("Follow the line")
-    drive_on_white = DriveOnWhiteBehaviour()
-    drive_on_white.setup(0, brain)
-    search_the_white = SearchTheWhiteBehaviour()
-    search_the_white.setup(0, brain)
-    follow_the_line.add_children([drive_on_white, search_the_white])
+    activate_yellow = ActivateYellowBehaviour()
+    activate_yellow.setup(0, brain)
+    activate_yellow_oneshot = py_trees.decorators.OneShot(activate_yellow)
+    the_line = FollowTheLineBehaviour()
+    the_line.setup(0, brain)
+    follow_the_line.add_children([activate_yellow_oneshot, the_line])
     the_cache.add_children([entry_run_oneshot, follow_the_line])
     return the_cache
